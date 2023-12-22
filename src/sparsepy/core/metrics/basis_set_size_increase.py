abe69c91e21658389e6e0c4acfc3293fb6faac14
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2023-12-22 18:39:21 +0530

    clean up basis set size metrics

--- src/sparsepy/core/metrics/basis_set_size_increase.py
+++ src/sparsepy/core/metrics/basis_set_size_increase.py
@@ -1,35 +1,43 @@
 import torch
 import numpy
+
 from sparsepy.access_objects.models.model import Model
 from sparsepy.core.model_layers.sparsey_layer import MAC, SparseyLayer
 from sparsepy.core.hooks import LayerIOHook
 from sparsepy.core.metrics.metrics import Metric
 
+
 class BasisSetSizeIncreaseMetric(Metric):
-
-    old_sizes = None
-
+    """
+    BasisSetSizeIncreaseMetric: metric to keep track
+        of basis set sizes across a Sparsey model.
+    """
     def __init__(self, model: torch.nn.Module):
         super().__init__(model)
         self.old_sizes = self._get_set_sizes(model)
 
-    def compute(self, m: Model, last_batch: torch.Tensor, labels: torch.Tensor, training: bool = True):
+    def compute(self, m: Model, last_batch: torch.Tensor,
+                labels: torch.Tensor, training: bool = True):
 
-        # non-recursive version will need to be replaced for nested models
+        # non-recursive version will need to be replaced
+        # for nested models
         # once we solve the nondeterministic return from m.modules()
         # compute sizes in the same way as BasisSetSize
         new_sizes = self._get_set_sizes(m)
 
-        #new_sizes = [
-        #    [len(mac.stored_codes) for mac in layer.children() if isinstance(mac, MAC)] for layer in m.children()
-        #]
-
-        # if you don't train your own model, this will produce an error on run 0
+        # if you don't train your own model,
+        # this will produce an error on run 0
         # then get the change in basis for each MAC
         #if old_sizes:
-        deltas = numpy.subtract(new_sizes, self.old_sizes)
-        #else:
-        #    deltas = new_sizes
+        deltas = []
+
+        for layer_index in range(len(new_sizes)):
+            deltas.append(
+                numpy.subtract(
+                    new_sizes[layer_index],
+                    self.old_sizes[layer_index]
+                )
+            )
 
         # the current basis set is the new previous basis set
         self.old_sizes = new_sizes
@@ -37,9 +45,10 @@ class BasisSetSizeIncreaseMetric(Metric):
         # return the differences
         return deltas
     
+
     def _get_set_sizes(self, m: Model):
-        #return [
-        #    [(len(gc.stored_codes) or 0) for gc in child.children() if isinstance(gc, MAC)] for child in m.children() if isinstance(child, SparseyLayer)
-        #]
-        return [[len(mac.stored_codes) for mac in layer.mac_list] for layer in m.children() if isinstance(layer, SparseyLayer)]
+        return [
+            [len(mac.stored_codes) for mac in layer.mac_list]
+            for layer in m.children() if isinstance(layer, SparseyLayer)
+        ]
     
