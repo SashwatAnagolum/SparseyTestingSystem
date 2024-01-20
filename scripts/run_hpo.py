c7584e63a9d75d3c747424b59f7f38106b512d5c
ajk7012 <99841243+ajk7012@users.noreply.github.com>
2024-01-20 00:12:18 -0500

    made initial read in hpo config script (argparse)

---
+++ scripts/run_hpo.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+Run HPO: script to run HPO
+"""
+
+import argparse
+
+from sparsepy.cli.config_validation.validate_config import (
+    validate_config, get_config_info
+)
+
+from sparsepy.tasks.train_model import train_model
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parses the command line arguments passed in during execution.
+
+    Returns:
+        Namespace containing the parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--model_config', type=str,
+        help='The location of the model config file.'
+    )
+
+    parser.add_argument(
+        '--training_recipe_config', type=str,
+        help='The location of the training recipe config file.'
+    )
+
+    parser.add_argument(
+        '--preprocessing_config', type=str,
+        help='The location of the preprocessing config file.'
+    )
+
+    parser.add_argument(
+        '--dataset_config', type=str,
+        help='The location of the dataset config file.'
+    )
+
+    parser.add_argument(
+        '--hpo_config', type=str,
+        help='The location of the hyperparameter optimization (HPO) config file.'
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    model_config_info = get_config_info(args.model_config)
+    validated_model_config, _ = validate_config(
+        model_config_info, 'model', 'sparsey'
+    )
+
+    training_recipe_config_info = get_config_info(
+        args.training_recipe_config
+    )
+    validated_trainer_config, _ = validate_config(
+        training_recipe_config_info, 'training_recipe', 'sparsey'
+    )
+
+    preprocessing_config_info = get_config_info(
+        args.preprocessing_config
+    )
+
+    dataset_config_info = get_config_info(
+        args.dataset_config
+    )
+    validated_dataset_config, _ = validate_config(
+        dataset_config_info, 'dataset', 'image'
+    )
+
+    hpo_config_info = get_config_info(
+        args.hpo_config
+    )
+
+    # TODO update to validate the config
+    validated_hpo_config = hpo_config_info
+
+    # TODO start the HPO run
+
+if __name__ == "__main__":
+    main()
