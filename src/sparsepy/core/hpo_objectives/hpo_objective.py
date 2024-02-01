91357f9ee16a30f068de0df0d11f29f5ab2f200b
ajk7012 <99841243+ajk7012@users.noreply.github.com>
2024-02-01 03:52:53 -0500

    initial structure for HPO Objective class

---
+++ src/sparsepy/core/hpo_objectives/hpo_objective.py
@@ -0,0 +1,31 @@
+import torch
+from sparsepy.core.metrics.metric_factory import MetricFactory
+class HPOObjective:
+    def __init__(self, hpo_config: dict):
+        self.metrics = []
+        self.weights = []
+        if hpo_config:
+            self.process_hpo_config(hpo_config)
+
+
+    def add_objective(self, name, weight):
+        self.metrics.append(name)
+        self.weights.append(weight)
+
+    def process_hpo_config(self, hpo_config):
+        """
+        Processes the HPO configuration and populates the objectives.
+
+        :param hpo_config: A dictionary representing the parsed HPO configuration.
+        """
+        for objective in hpo_config.get("optimization_objective", []):
+            name = objective.get("name")
+            weight = objective.get("weight")
+            params = objective.get("params")
+            if name and weight is not None:  # Ensures both name and weight are present
+                if isinstance(params, dict):
+                    metric = MetricFactory.create_metric(name, **params)
+                else:
+                    metric = MetricFactory.create_metric(name)
+                self.add_objective(metric, weight)
+
