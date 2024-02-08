1c88c2e6cfb3eb9e8c56bd31f2f43f2d0e395a54
JD Padrnos <jdpadrnos@gmail.com>
2024-02-08 13:09:08 -0800

    consider only active MACs

--- src/sparsepy/core/metrics/feature_coverage.py
+++ src/sparsepy/core/metrics/feature_coverage.py
@@ -74,33 +74,31 @@ class FeatureCoverageMetric(Metric):
                     source_macs = mac.input_filter
 
                     #print(f"MAC {mac_index} Sources: {[x for x, y in enumerate(source_macs) if y != 0]}")
-                    
-                    # TODO only consider MACs that are currently active as part of the mask building
 
-                    # IF we are in layer 1 then the source "MACs" represent pixels in the input that we need to scatter onto the mask
-                    if layer_index == 0:
-                        mac_mask.scatter_(0, source_macs, 1)
-                        #print(f"Mask shape: {mac_mask.shape}")
-                        #print(f"{mac_mask}")
+                    # if this MAC is active (=any nonzero neuron outputs = any nonzero values in the 2D tensor for this MAC's output)
+                    if layer_outputs[macs_counted][image_index].count_nonzero(dim=[0,1]) > 0:
 
-                    # ELSE loop over all the source MACs
-                    else:
-                    # for each of those source MACs
-                        for source_index in source_macs:
-                            # get its input filter from the cache and OR it into the mac_mask
-                            # WARNING double check index numbering!
-                            mac_mask = torch.bitwise_or(mac_mask, rf_cache[layer_index - 1][source_index])
-        
-                    # save to the cache as the input mask of this MAC
-                    # TODO for MACs that are NOT active append a blank mask to the cache
-                    rf_cache[layer_index].append(mac_mask)
-                    #rf_cache[layer_index][mac_index] = mac_mask
-                    #print(mac_mask)
-                    # IF this MAC is active
-                    if layer_outputs[macs_counted][image_index].any()  > 0:
-                        # then OR its finished input mask into this layer's input mask
+                        # then update the MAC input mask with this MAC's input
+
+                        # IF we are in layer 1 then the source "MACs" represent pixels in the input that we need to scatter onto the mask
+                        if layer_index == 0:
+                            mac_mask.scatter_(0, source_macs, 1)
+                        # ELSE loop over all the source MACs
+                        else:
+                            # for each of those source MACs
+                            for source_index in source_macs:
+                                # get its input filter from the cache and OR it into the mac_mask
+                                # this is slightly inefficient because it does (# of MACs) fetch/OR steps rather than (# of active MACs)
+                                # WARNING double check index numbering!
+                                mac_mask = torch.bitwise_or(mac_mask, rf_cache[layer_index - 1][source_index])
+
+                        # then OR this MAC into the current layer mask
                         layer_mask = torch.bitwise_or(layer_mask, mac_mask)
-                    # regardless of activity, increment MAC counter
+
+                    # regardless, save to the cache as the input mask for this MAC
+                    # (this way, MACs that are not active get an all-zero mask)
+                    rf_cache[layer_index].append(mac_mask)
+                    # and increment the MAC counter
                     macs_counted += 1
 
                 # when MAC processing has finished
@@ -109,20 +107,28 @@ class FeatureCoverageMetric(Metric):
                 covered_pixels = torch.logical_and(layer_mask, image)
 
                 # count the number of 1s in the covered pixels
-                covered_count = torch.count_nonzero(covered_pixels)
+                covered_count = torch.count_nonzero(covered_pixels).item()
 
                 # XOR this with the active pixels to get the uncovered pixels
                 uncovered_pixels = torch.logical_xor(covered_pixels, image)
 
                 # count the number of 1s in the uncovered pixels
-                uncovered_count = torch.count_nonzero(uncovered_pixels)
+                uncovered_count = torch.count_nonzero(uncovered_pixels).item()
         
+                total_count = image.count_nonzero().item()
+
                 # at Level 3 verbosity print the uncovered pixel mask
                 print(f"Layer {layer_index}: covered {covered_count} uncovered {uncovered_count}")
 
-                # calculate feature coverage in results as covered pixels / input size
-                # WARNING unsafe division
-                feature_coverage = (covered_count / image.count_nonzero()).item()
+                # handle the case of an empty input
+                if total_count == 0:
+                    # if so we have "covered" all zero features
+                    feature_coverage = 1.0
+                else:
+                    # calculate feature coverage in results as covered pixels / input size
+                    feature_coverage = (covered_count / total_count) if total_count > 0 else 1
+                
+                # append to results for this image
                 image_results.append(feature_coverage)
 
             results.append(image_results) # confirm dimensions are consistent with other metrics
