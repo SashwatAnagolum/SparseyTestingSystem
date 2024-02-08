55a7993c7f6767c14c917eb68b59e3e436614d7f
JD Padrnos <jdpadrnos@gmail.com>
2024-02-08 15:10:43 -0800

    layerIO returns layerwise (but disabled by default for compatibility)

--- src/sparsepy/core/hooks/layer_io.py
+++ src/sparsepy/core/hooks/layer_io.py
@@ -7,7 +7,10 @@ Layer IO: file hlding the LayerIOHook class.
 
 import torch
 
+from itertools import chain
+
 from sparsepy.core.hooks.hook import Hook
+from sparsepy.core.model_layers.sparsey_layer import SparseyLayer
 
 
 class LayerIOHook(Hook):
@@ -15,7 +18,7 @@ class LayerIOHook(Hook):
     Layer IO Hook: simple hook to get the output
         and input of a layer.
     """
-    def __init__(self, module: torch.nn.Module) -> None:
+    def __init__(self, module: torch.nn.Module, flatten = True) -> None:
         """
         Initializes the hook, and registers it with the model.
 
@@ -24,9 +27,20 @@ class LayerIOHook(Hook):
         """
         super().__init__(module)
 
+        # runs before forward is invoked (only on top-level Model)
+        # on pre hook, reinitialize the lists
         self.input_list = []
         self.output_list = []
         self.layer_list = []
+
+        self.flatten = flatten
+
+        for m in module.modules():
+            if isinstance(m, SparseyLayer):
+                # WARNING this approach will not work correctly with nested SparseyLayers
+                self.input_list.append([])
+                self.output_list.append([])
+                self.layer_list.append([])
 
     def hook(self) -> None:
         """
@@ -47,9 +61,17 @@ class LayerIOHook(Hook):
 
 
     def pre_hook(self, module: torch.nn.Module, input: torch.Tensor) -> None:
-        self.layer_list = []
         self.input_list = []
         self.output_list = []
+        self.layer_list = []
+
+        # creates implicit requirement that the module passed to the pre hook is the top-level module
+        for m in module.modules():
+            if isinstance(m, SparseyLayer):
+                # WARNING this approach will not work correctly with nested SparseyLayers
+                self.input_list.append([])
+                self.output_list.append([])
+                self.layer_list.append([])
 
 
     def forward_hook(self, module: torch.nn.Module,
@@ -63,10 +85,13 @@ class LayerIOHook(Hook):
             input (torch.Tensor): module input
             output (torch.Tensor): module output
         """
-        self.layer_list.append(module)
-        self.output_list.append(output)
-        self.input_list.append(input[0])
+        self.layer_list[module.layer_index].append(module)
+        self.output_list[module.layer_index].append(output)
+        self.input_list[module.layer_index].append(input[0])
 
 
     def get_layer_io(self):
-        return self.layer_list, self.input_list, self.output_list
+        if self.flatten:
+            return list(chain.from_iterable(self.layer_list)), list(chain.from_iterable(self.input_list)), list(chain.from_iterable(self.output_list))
+        else:
+            return self.layer_list, self.input_list, self.output_list
