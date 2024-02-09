25a1ce713f4ec22021011b29f2c3262335f04ca9
JD Padrnos <jdpadrnos@gmail.com>
2024-02-08 16:29:30 -0800

    use new layerio structure in optimizer

--- src/sparsepy/core/optimizers/hebbian.py
+++ src/sparsepy/core/optimizers/hebbian.py
@@ -19,6 +19,7 @@ class HebbianOptimizer(torch.optim.Optimizer):
         super().__init__(model.parameters(), dict())
         self.model = model
         self.saturation_thresholds = []
+        # BUG this will not operate correctly in certain kinds of model
         for layer in model.children():
             if hasattr(layer, 'saturation_threshold'):
                 self.saturation_thresholds.append(layer.saturation_threshold)
@@ -28,7 +29,7 @@ class HebbianOptimizer(torch.optim.Optimizer):
 
         self.verbosity = 0
 
-        self.hook = LayerIOHook(self.model)
+        self.hook = LayerIOHook(self.model, flatten=False)
 
     def calculate_freezing_mask(self, weights, layer_index):
         #Retrieve the threshold for freezing weights for the current layer from a list of thresholds.
@@ -76,56 +77,55 @@ class HebbianOptimizer(torch.optim.Optimizer):
         # 'inputs' and 'outputs' are tensors representing inputs and outputs for those MACs.
         layers, inputs, outputs = self.hook.get_layer_io()
 
-        # Iterate over each layer along with its input and output tensors.
-        for (mac_index, (layer, layer_input, layer_output)) in enumerate(zip(layers, inputs, outputs)):
-            # Iterate over each parameter tensor within the current layer.
-            for params in layer.parameters():
-                # Calculate weight updates by performing matrix multiplication between
-                # the transpose of the flattened layer input and the flattened layer output.
-                weight_updates = torch.matmul(
-                    torch.transpose(
-                        layer_input.view(layer_input.shape[0], -1), 0, 1
-                    ),
-                    layer_output.view(layer_output.shape[0], -1)
-                )
+        # Iterate over each layer 
+        for layer_index, layer in enumerate(layers):
+            # iterate over each MAC along with its input and output tensors.
+            for (mac_index, (mac, mac_input, mac_output)) in enumerate(zip(layer, inputs[layer_index], outputs[layer_index])):
+                # iterate over the parameters of the current MAC
+                for params in mac.parameters():
+                    # Calculate weight updates by performing matrix multiplication between
+                    # the transpose of the flattened layer input and the flattened layer output.
+                    weight_updates = torch.matmul(
+                        torch.transpose(
+                            mac_input.view(mac_input.shape[0], -1), 0, 1
+                        ),
+                        mac_output.view(mac_output.shape[0], -1)
+                    )
 
-                # Reshape the weight updates to match the dimensions of the parameters
-                # and then permute the dimensions for correct alignment.
-                weight_updates = torch.permute(
-                    weight_updates.view(
-                        weight_updates.shape[0],
-                        params.shape[0],
-                        params.shape[2]
-                    ),
-                    (1, 0, 2)
-                )
+                    # Reshape the weight updates to match the dimensions of the parameters
+                    # and then permute the dimensions for correct alignment.
+                    weight_updates = torch.permute(
+                        weight_updates.view(
+                            weight_updates.shape[0],
+                            params.shape[0],
+                            params.shape[2]
+                        ),
+                        (1, 0, 2)
+                    )
 
-                # Retrieve the index of the current layer to access its specific threshold.
-                layer_index = layer.layer_index
+                    # Print the current parameter values for investigation.
+                    # Note: Converting to NumPy for easier visualization.
+                    if self.verbosity > 0:
+                        print("Params.data " + str(params.data.numpy()))
 
-                # Print the current parameter values for investigation.
-                # Note: Converting to NumPy for easier visualization.
-                if self.verbosity > 0:
-                    print("Params.data " + str(params.data.numpy()))
+                    # Calculate the updateable mask based on the current parameters and layer's threshold.
+                    print("Layer: " + str(layer_index))
+                    print("MAC: " + str(mac_index))
+                    updateable_mask = self.calculate_freezing_mask(params.data, layer_index)
+                    print("\n")
 
-                # Calculate the updateable mask based on the current parameters and layer's threshold.
-                print("Layer: " + str(layer_index))
-                print("MAC: " + str(mac_index))
-                updateable_mask = self.calculate_freezing_mask(params.data, layer_index)
-                print("\n")
+                    # Apply the updateable mask to the weight updates, effectively zeroing
+                    # updates for weights that are not updateable (frozen).
+                    # BUG: probably does not update weights that are both active on this step *and* frozen
+                    weight_updates *= updateable_mask
+                    
+                    # apply persistence/weight decay to all weights 
+                    # (newly changed weights will be reset to 1 in the next step)
+                    # CHECK whether we need to ignore the frozen weights for decay; if so more will be needed...
+                    params = torch.mul(params, mac.persistence)
 
-                # Apply the updateable mask to the weight updates, effectively zeroing
-                # updates for weights that are not updateable (frozen).
-                # BUG: probably does not update weights that are both active on this step *and* frozen
-                weight_updates *= updateable_mask
-                
-                # apply persistence/weight decay to all weights 
-                # (newly changed weights will be reset to 1 in the next step)
-                # CHECK whether we need to ignore the frozen weights for decay; if so more will be needed...
-                params = torch.mul(params, layer.persistence)
-
-                # add the new weights to the old ones then clamp to [0,1]
-                params += torch.ge(weight_updates, 1)
-                torch.clamp(params, 0, 1, out=params)
-
+                    # add the new weights to the old ones then clamp to [0,1]
+                    params += torch.ge(weight_updates, 1)
+                    torch.clamp(params, 0, 1, out=params)
+            
         return
