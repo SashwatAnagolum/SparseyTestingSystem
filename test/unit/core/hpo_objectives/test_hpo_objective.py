92fa507e7500161595f2c462f69be2cb7e47385c
ajk7012 <99841243+ajk7012@users.noreply.github.com>
2024-02-14 01:17:32 -0500

    Update test_hpo_objective.py to use total

--- test/unit/core/hpo_objectives/test_hpo_objective.py
+++ test/unit/core/hpo_objectives/test_hpo_objective.py
@@ -60,4 +60,33 @@ class TestHPOObjective:
         # Calculate combined metric
         combined_metric = hpo_objective.combine_metrics(test_metric_data)
         # assert metric values
-        assert combined_metric == 1.305
+        assert combined_metric['total'] == 1.3050000000000002
+        test_metric_data = [
+            {
+                'ApproximateMatchAccuracyMetric': 5,
+                'BasisSetSizeIncreaseMetric': [np.array([3,5,5,7]), np.array([1,9,1,9])]
+            },
+            {
+                'ApproximateMatchAccuracyMetric': 3,
+                'BasisSetSizeIncreaseMetric': [np.array([1,5,1,5]), np.array([2,4,2,4])]
+            },
+            # Add more entries as needed
+        ]
+
+        # construct test HPO configuration
+        hpo_config = {
+            "optimization_objective": {
+                "combination_method": "mean",
+                "objective_terms": [
+                    {'metric': {"name": "approximate_match_accuracy"}, "weight": 1.0},
+                    {'metric': {"name": "basis_set_size_increase"}, "weight": 0.5}
+                ]
+            }
+        }
+
+        # Initialize HPOObjective with the configuration
+        hpo_objective = HPOObjective(hpo_config)
+
+        # Calculate combined metric
+        combined_metric = hpo_objective.combine_metrics(test_metric_data)
+        assert combined_metric['total'] == 3
