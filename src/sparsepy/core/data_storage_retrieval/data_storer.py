ba4cb1240bf9c70d917023f03389b8d5739dc033
ajk7012 <99841243+ajk7012@users.noreply.github.com>
2024-03-05 23:07:31 -0500

    basic skeletons for data_storage_retrieval

---
+++ src/sparsepy/core/data_storage_retrieval/data_storer.py
@@ -0,0 +1,36 @@
+import wandb
+from sparsepy.core.results import TrainingResult, TrainingStepResult, EvaluationResult, HPOResult, HPOStepResult
+from sparsepy.access_objects.models.model import Model
+
+"""
+DataStorer: Stores data to weights and biases
+"""
+class DataStorer:
+
+    def save_model(self, m: Model):
+        # Implementation to save the model
+        pass
+    
+    def save_training_step(self, parent: str, result: TrainingStepResult):
+        # Implementation to save the training step result
+        pass
+    
+    def save_training_result(self, result: TrainingResult):
+        # Implementation to save the training result
+        pass
+    
+    def save_evaluation_result(self, result: EvaluationResult):
+        # Implementation to save the evaluation result
+        pass
+    
+    def save_hpo_step(self, parent: str, result: HPOStepResult):
+        # Implementation to save the HPO step result
+        pass
+    
+    def save_hpo_result(self, result: HPOResult):
+        # Implementation to save the HPO result
+        pass
+    
+    def create_artifact(self, content: dict) -> wandb.Artifact:
+        # Implementation to create and save a wandb.Artifact
+        pass
