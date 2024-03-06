3acf4dbe263c617e0af01f4b6608e3a85765732e
ajk7012 <99841243+ajk7012@users.noreply.github.com>
2024-03-05 22:01:54 -0500

    updated best_runs

--- src/sparsepy/core/results/hpo_result.py
+++ src/sparsepy/core/results/hpo_result.py
@@ -5,13 +5,13 @@ class HPOResult(Result):
     def __init__(self, configs: dict, id: str, name: str):
         self.name = name
         self.id = id
-        self.best_run_id = None
+        self.best_run = None
         self.runs = []  # List of HPOStepResult objects
         self.configs = configs
 
     def add_step(self, step: HPOStepResult):
-        if self.best_run_id is None or step.get_objective()["total"] > self.runs[self.best_run_id].get_objective()["total"]:
-            self.best_run_id = step.id
+        if self.best_run is None or step.get_objective()["total"] > self.best_run.get_objective()["total"]:
+            self.best_run = step
         self.runs.append(step)
 
     def get_steps(self) -> list[HPOStepResult]:
