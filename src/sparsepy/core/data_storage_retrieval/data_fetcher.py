98d0be1e67696a0e526dc47777fa78971c33e1a5
JD Padrnos <jdpadrnos@gmail.com>
2024-03-07 23:35:37 -0800

    Firestore setup in DataFetcher and implementation of single-experiment fetch

--- src/sparsepy/core/data_storage_retrieval/data_fetcher.py
+++ src/sparsepy/core/data_storage_retrieval/data_fetcher.py
@@ -2,12 +2,29 @@
 """
 DataFetcher: Fetches data from weights and biases
 """
+
+import pickle
+
+from firebase_admin import firestore
+
+import wandb
+
 from sparsepy.core.results.training_result import TrainingResult
+from sparsepy.core.results.training_step_result import TrainingStepResult
 from sparsepy.core.results.evaluation_result import EvaluationResult
 from sparsepy.core.results.hpo_result import HPOResult
+from sparsepy.core.results.hpo_step_result import HPOStepResult
 
 class DataFetcher:
     """Fetches data related to sparsey models and results."""
+
+    def __init__(self):
+        
+        # create API client
+        self.api = wandb.Api()
+
+        # connect to Firestore
+        self.db = firestore.client()
     
     def get_model_weights(self, model_id: str) -> dict:
         """
@@ -31,7 +48,32 @@ class DataFetcher:
         Returns:
             TrainingResult: This Experiment's TrainingResult from w&b
         """
-        pass
+
+        # fetch this experiment's data back from Firestore
+        experiment_ref = self.db.collection("experiments").document(experiment_id)
+        # this fetches ALL the data for the object and should be used sparingly
+        experiment = experiment_ref.get().to_dict()
+
+        # create a new TrainingResult object to hold the retrieved data
+        tr = TrainingResult(experiment_id, experiment["saved_metrics"]["resolution"])
+
+        # populate all the training steps
+        for train_step in experiment["saved_metrics"]["training"]:
+            tsr = TrainingStepResult(experiment["saved_metrics"]["resolution"])
+            tsr.metrics = [
+                {
+                    met_name:pickle.loads(encoded_val) for met_name, encoded_val in train_step.items()
+                }
+            ]
+            tr.add_step(tsr)
+
+        # populate summary-level data: start, end, best steps
+        tr.start_time = experiment["start_time"]
+        tr.end_time = experiment["end_time"]
+        # FIXME implement best step retrieval
+        #tr.best_steps = experiment["best_steps"]
+
+        return tr
 
     def get_eval_result(self, experiment_id: str) -> EvaluationResult:
         """
@@ -43,7 +85,6 @@ class DataFetcher:
         Returns:
             EvaluationResult: the EvaluationResult for the experiment of this id in w&b
         """
-        pass
     
     def get_hpo_result(self, experiment_id: str) -> HPOResult:
         """
