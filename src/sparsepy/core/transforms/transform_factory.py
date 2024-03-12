d4af62280470ed5c6fb6f41d55989ef1b8fc8494
JD Padrnos <jdpadrnos@gmail.com>
2024-03-11 17:53:33 -0700

    fix TransformFactory reflection for our own and torchvision v2 transforms

---
+++ src/sparsepy/core/transforms/transform_factory.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+"""
+Transform Factory: class to build transforms.
+"""
+
+import inspect
+
+import torch
+
+from torchvision.transforms import v2
+
+from sparsepy.core import transforms
+
+
+class TransformFactory:
+    allowed_modules = set([i for i in dir(transforms) if i[:2] != '__'])
+
+    @staticmethod
+    def get_transform_name(transform_name):
+        class_name = ''.join(
+            [l.capitalize() for l in transform_name.split('_')]
+        )
+
+        return class_name
+
+    @staticmethod
+    def get_transform_class(class_name):
+        """
+        Gets the class corresponding to the name passed in.
+        Throws an error if the name is not valid.
+        """
+        # input is TransformName but standard for our classes is TransformNameTransform
+        if class_name + 'Transform' in TransformFactory.allowed_modules:
+            return getattr(transforms, class_name + 'Transform')
+        # torchvision modules contain multiple classes; use reflection to get all the
+        # torchvision class members' names and see if ours is among them
+        elif class_name in [cls[0] for cls in inspect.getmembers(v2, inspect.isclass)]:
+            return getattr(v2, class_name)
+        else:
+            raise ValueError(f'Invalid transform name: {class_name}!')
+    
+
+    @staticmethod
+    def create_transform(transform_name, **kwargs) -> v2.Transform:
+        """
+        Creates a transform passed in based on the transform name and kwargs.
+        """
+        transform_name = TransformFactory.get_transform_name(transform_name)
+
+        transform_class = TransformFactory.get_transform_class(transform_name)
+
+        if (transform_name in dir(v2)) and ('dtype' in kwargs):
+            kwargs['dtype'] = getattr(torch, kwargs['dtype'])
+
+        transform_obj = transform_class(**kwargs)
+
+        return transform_obj
