9e8cb4e60c1531008b1855c6e4ddba900334dcf0
SashwatAnagolum <sashwat.anagolum@gmail.com>
2024-04-06 17:22:34 +0530

    Remove caching and make saving to disk optional in PreprocessedDataset

--- src/sparseypy/access_objects/datasets/preprocessed_dataset.py
+++ src/sparseypy/access_objects/datasets/preprocessed_dataset.py
@@ -4,7 +4,6 @@ Preprocessed Dataset: wrapper for datasets
 """
 import os
 import pickle
-import functools
 from sparseypy.access_objects.datasets.dataset import Dataset
 from sparseypy.access_objects.preprocessing_stack.preprocessing_stack import PreprocessingStack
 
@@ -18,50 +17,56 @@ class PreprocessedDataset(Dataset):
         preprocessed_flags (list[bool]): A boolean list indicating whether an item has been preprocessed.
     """
 
-    def __init__(self, dataset: Dataset, preprocessing_stack: PreprocessingStack, preprocessed_dir: str = "datasets/preprocessed_dataset"):
+    def __init__(self, dataset: Dataset,
+                 preprocessing_stack: PreprocessingStack,
+                 preprocessed_dir: str = "datasets/preprocessed_dataset",
+                 save_to_disk: bool = False):
         """
         Initialize the PreprocessedDataset.
+
         Args:
             dataset (Dataset): The dataset to be preprocessed.
             preprocessed_dir (str): Directory to store preprocessed data.
             preprocessing_stack (PreprocessingStack): Stack of preprocessing steps to apply.
+            save_to_disk (bool): 
         """
+        super().__init__()
+
         self.dataset = dataset
         self.preprocessed_dir = preprocessed_dir
         self.preprocessing_stack = preprocessing_stack
-        self.preprocessed_flags = [False] * len(dataset)  # Initialize all flags as False
+        self.save_to_disk = save_to_disk
 
         # Create the directory for preprocessed data if it does not exist
-        if not os.path.exists(self.preprocessed_dir):
-            os.makedirs(self.preprocessed_dir)
+        if self.save_to_disk:
+            self.saved_to_disk_flags = [False for i in len(dataset)]
+            
+            if not os.path.exists(self.preprocessed_dir):
+                os.makedirs(self.preprocessed_dir)
 
-    def _preprocess_and_save(self, idx):
+
+    def save_data_to_disk(self, data, label, idx) -> None:
         """
-        Preprocess and save data for a given index.
+        Save data for a given index.
+
         Args:
+            data (torch.Tensor): the data to save.
+            label (torch.Tensor): the label to save.
             idx (int): Index of the data in the dataset.
-        Returns:
-            Tuple containing preprocessed data and its label.
         """
-        # Retrieve data and label from the original dataset
-        data, label = self.dataset[idx]
-
-        # Apply preprocessing steps
-        preprocessed_data = self.preprocessing_stack(data)
-
         # Path where the preprocessed data will be saved
-        preprocessed_path = os.path.join(self.preprocessed_dir, f'{idx}.pkl')
+        preprocessed_path = os.path.join(
+            self.preprocessed_dir, f'{idx}.pkl'
+        )
 
         # Save the preprocessed data and label
         with open(preprocessed_path, 'wb') as f:
-            pickle.dump((preprocessed_data, label), f)
+            pickle.dump((data, label), f)
 
         # Mark this item as preprocessed in the boolean array
-        self.preprocessed_flags[idx] = True
+        self.saved_to_disk_flags[idx] = True
 
-        return preprocessed_data, label
-    
-    @functools.lru_cache(maxsize=None)
+
     def __getitem__(self, idx):
         """
         Get item by index, applying preprocessing if necessary.
@@ -70,23 +75,34 @@ class PreprocessedDataset(Dataset):
         Returns:
             Preprocessed data and its label.
         """
-        # Path to the preprocessed file for this index
-        preprocessed_path = os.path.join(self.preprocessed_dir, f'{idx}.pkl')
+        if self.save_to_disk:
+            # Path to the preprocessed file for this index
+            preprocessed_path = os.path.join(
+                self.preprocessed_dir, f'{idx}.pkl'
+            )
 
-        # Check if this item has been preprocessed
-        if self.preprocessed_flags[idx]:
-            # If preprocessed, try to load the data from the file
-            try:
-                with open(preprocessed_path, 'rb') as f:
-                    data, label = pickle.load(f)
-            except (IOError, EOFError) as e:
-                # Handle file reading errors
-                raise Exception(f"Error reading file {preprocessed_path}: {e}")
+            if self.saved_to_disk_flags[idx]:
+                # If preprocessed, try to load the data from the file
+                try:
+                    with open(preprocessed_path, 'rb') as f:
+                        data, label = pickle.load(f)
+                except (IOError, EOFError) as e:
+                    # Handle file reading errors
+                    raise Exception(
+                        f"Error reading file {preprocessed_path}: {e}"
+                    )
+            else:
+                # If not preprocessed, preprocess and save the data
+                data, label = self.dataset.__get__(idx)
+                data = self.preprocessing_stack(data)
+
+                self.save_data_to_disk(data, label, idx)
         else:
-            # If not preprocessed, preprocess and save the data
-            data, label = self._preprocess_and_save(idx)
+            data, label = self.dataset.__get__(idx)
+            data = self.preprocessing_stack(data)
 
         return data, label
+
 
     def __len__(self):
         """
