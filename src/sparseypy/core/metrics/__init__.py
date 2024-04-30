c38d48a13ea1f1fcf6d32a4a7d1494888fd6e1e4
JD Padrnos <jdpadrnos@gmail.com>
2024-04-30 16:59:31 -0700

    remove vestiges of BSSI

--- src/sparseypy/core/metrics/__init__.py
+++ src/sparseypy/core/metrics/__init__.py
@@ -1,6 +1,5 @@
 from .match_accuracy import MatchAccuracyMetric
 from .basis_set_size import BasisSetSizeMetric
-from .basis_set_size_increase import BasisSetSizeIncreaseMetric
 from .feature_coverage import FeatureCoverageMetric
 from .basis_average import BasisAverageMetric
 from .num_activations import NumActivationsMetric
