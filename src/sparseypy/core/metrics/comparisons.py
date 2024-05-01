9b91845293212b50d7981582c34fa5fe34f0ad54
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2024-05-01 15:56:13 +0530

    Move average_nested_data to new reductions file

--- src/sparseypy/core/metrics/comparisons.py
+++ src/sparseypy/core/metrics/comparisons.py
@@ -2,28 +2,8 @@
 comparsions.py - contains comparison functions for determining the "best" value of a metric
 """
 
-import numpy as np
-import torch
 
-#### SUPPORT FUNCTIONS ####
-def average_nested_data(data: torch.Tensor):
-    """
-    Averages an arbitrarily deep data structure
-    and returns the result as a single value.
-
-    Used here to reduce the granularity of data in order
-    to store a single value for each step in W&B.
-
-    Args:
-        data (torch.Tensor): the (possibly nested) tensor
-            containing the raw metric values computed.
-
-    Returns:
-        (float): a single value representing the averaged data
-    """
-    return torch.mean(
-        torch.stack([torch.mean(t) for t in data.unbind()])
-    ).cpu().item()
+from sparseypy.core.metrics.reductions import average_nested_data
 
 
 #### BUILT IN COMPARISON FUNCTIONS ####
